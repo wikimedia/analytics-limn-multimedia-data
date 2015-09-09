2bb4e811dd5d2f8821dcf55986965f21db441039
milimetric <dandreescu@wikimedia.org>
2015-09-09 10:20:37 -0400

    Add basic structure
    Change-Id: I890f1ab742b71b94308ac0bdfeb4b8f14baa44df

---
+++ multimedia/uploads.sql
@@ -0,0 +1,7 @@
+-- TODO: implement this (... is not valid SQL :))
+ select date('{from_timestamp}') as date,
+        count(*) as {wiki_db}
+   from {wiki_db}....
+  where ...
+        ... probably use from_timestamp and to_timestamp
+;

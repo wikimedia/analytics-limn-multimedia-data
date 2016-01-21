e21caab200aa658070c07147fb2dd7f15eb9015d
milimetric <dandreescu@wikimedia.org>
2016-01-21 12:59:05 -0500

    Improve speed of new uploaders a bit
    Change-Id: I14aca3ac15a70fb7315e35700dc8950f7870eed1

--- multimedia/new-uploaders.sql
+++ multimedia/new-uploaders.sql
@@ -1,15 +1,9 @@
-select date('{from_timestamp}') as date,
-	count(i.img_user) as {wiki_db}
-	from (
-		select img_user,
-			min(img_timestamp) as timestamp
-			from {wiki_db}.image
-			group by img_user
-	) firsts
-
-	join {wiki_db}.image i
-		on firsts.img_user = i.img_user
-		and firsts.timestamp = i.img_timestamp
-	
-	where i.img_timestamp >= '{from_timestamp}' and
-		i.img_timestamp < '{to_timestamp}';
+ select date('{from_timestamp}') as date,
+        count(*) as {wiki_db}
+   from (select img_user
+           from {wiki_db}.image
+          group by img_user
+         having min(img_timestamp) >= '{from_timestamp}'
+            and min(img_timestamp) < '{to_timestamp}'
+        ) firsts_this_month
+;

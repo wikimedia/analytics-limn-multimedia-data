cfba9dc8f34eab0f6bb6bef5735f06bf10d13e83
Mark Holmquist <mtraceur@member.fsf.org>
2016-01-25 08:08:17 -0600

    Fix cross-wiki upload script
    The recentchanges table only goes back 30 days, which is why we didn't
    have any useful historical data. MatmaRex set me straight, and pointed
    me to the change_tag table, which is less stupid than tag_summary.
    
    Change-Id: Ic6fe5ef178094c934621b84c498e120b08918bfb

--- multimedia/cross-wiki-uploads.sql
+++ multimedia/cross-wiki-uploads.sql
@@ -1,8 +1,9 @@
 select date('{from_timestamp}') as date,
 	count(*) as {wiki_db}
-	from {wiki_db}.tag_summary
-		join {wiki_db}.recentchanges on
-			ts_rc_id = rc_id
-	where ts_tags="cross-wiki-upload" and
-		rc_timestamp >= '{from_timestamp}' and
-		rc_timestamp < '{to_timestamp}';
+	from {wiki_db}.change_tag
+		join {wiki_db}.logging on
+			ct_log_id = log_id
+	where ct_tag = "cross-wiki-upload" and
+		log_type = "upload" and
+		log_timestamp >= '{from_timestamp}' and
+		log_timestamp < '{to_timestamp}';
